c78fe2d43fe0a64f70cffb920f54ef878e414c51
Marine <mariine.becker@gmail.com>
2023-03-12 14:52:58 +0100

    ajout de l'authentification

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ArticleController;
 
 /*
 |--------------------------------------------------------------------------
@@ -28,7 +28,6 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-
 Route::resource('lf_articles', ArticleController::class);
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
